3b38c925ab962095f59d0453e6769cf4c64ebd7b
sergeybv26 <sergey.bv26@gmail.com>
2022-10-21 16:26:53 +0300

    Создана графическая оболочка для сервера

--- server/run.py
+++ server/run.py
@@ -1,8 +1,13 @@
 """Модуль запуска для сервера"""
 from argparse import ArgumentParser
-from asyncio import get_event_loop
+from asyncio import get_event_loop, set_event_loop
+from sys import argv
+
+from quamash import QEventLoop
+from PyQt5.QtWidgets import QApplication
 
 from server.server_config import PORT, DB_PATH
+from server.ui.windows import ServerMonitorWindow
 from server.utils.server_proto import ChatServerProtocol
 
 
@@ -35,6 +40,40 @@ class ConsoleServerApp:
         loop.close()
 
 
+class GuiServerApp:
+    """Класс раннера сервера с графическим интерфейсом"""
+    def __init__(self, parsed_args, db_path):
+        self.args = parsed_args
+        self.db_path = db_path
+        self.ins = None
+
+    def main(self):
+        """Главный метод"""
+        connections = {}
+        users = {}
+
+        self.ins = ChatServerProtocol(self.db_path, connections, users)
+
+        app = QApplication(argv)
+        loop = QEventLoop(app)
+        set_event_loop(loop)
+        wnd = ServerMonitorWindow()
+        wnd.show()
+
+        with loop:
+            coro = loop.create_server(lambda: self.ins, self.args['addr'], self.args['port'])
+            server = loop.run_until_complete(coro)
+            print('Server on {} : {}'.format(*server.sockets[0].getsockname()))
+
+            try:
+                loop.run_forever()
+            except KeyboardInterrupt:
+                pass
+            server.close()
+            loop.run_until_complete(server.wait_closed())
+            loop.close()
+
+
 def parse_and_run():
     """Осуществляет парсинг аргументов и запускает сервер"""
 
@@ -52,6 +91,9 @@ def parse_and_run():
     if args['nogui']:
         # Запускаем консольный сервер
         app = ConsoleServerApp(args, DB_PATH)
+        app.main()
+    else:
+        app = GuiServerApp(args, DB_PATH)
         app.main()
 
 
---
+++ server/ui/windows.py
@@ -0,0 +1,12 @@
+"""Модуль, содержащий инициализацию окна серверного приложения"""
+from PyQt5.QtWidgets import QMainWindow
+
+from server.ui.server_monitor import Ui_ServerWindow as server_ui_class
+
+
+class ServerMonitorWindow(QMainWindow):
+    """Класс инициализации главного окна сервера"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = server_ui_class()
+        self.ui.setupUi(self)
